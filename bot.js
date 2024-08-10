9d9975467eb1fd3eb011fd41392e11bac2f4fc69
Mid0aria <infoihsandemir@gmail.com>
2024-08-10 19:45:19 +0300

    added termux setup

--- bot.js
+++ bot.js
@@ -125,3 +125,28 @@ const nitroRegex =
         chalk.green.bold(`Total ${totalGuilds} servers are listening.`)
     );
 })();
+
+process.on("unhandledRejection", (reason, p) => {
+    console.log(
+        chalk.blue(chalk.bold(`[antiCrash]`)),
+        chalk.white(`>>`),
+        chalk.magenta(`Unhandled Rejection/Catch`),
+        chalk.red(reason, p)
+    );
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log(
+        chalk.blue(chalk.bold(`[antiCrash]`)),
+        chalk.white(`>>`),
+        chalk.magenta(`Unhandled Exception/Catch`),
+        chalk.red(err, origin)
+    );
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(
+        chalk.blue(chalk.bold(`[antiCrash]`)),
+        chalk.white(`>>`),
+        chalk.magenta(`Uncaught Exception/Catch`),
+        chalk.red(err, origin)
+    );
+});
